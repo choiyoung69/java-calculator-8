5eea1fdb91901307eb051ee08e20868c9d60529d
choiyoung69 <choiyoung609@gmail.com>
2025-10-20 19:10:32 +0900

    refactor: CustomDelimiterStrategy에서 하드 코딩 제거

--- src/main/java/calculator/domain/CustomDelimiterStrategy.java
+++ src/main/java/calculator/domain/CustomDelimiterStrategy.java
@@ -4,16 +4,22 @@ import java.util.regex.Pattern;
 
 public class CustomDelimiterStrategy extends DefaultDelimiterStrategy {
 
+    private static final String CUSTOM_DELIMITER_PREFIX = "//";
+    private static final String CUSTOM_DELIMITER_SEPARATOR = "\\n";
+
     @Override
     public boolean canHandle(String input) {
-        return input != null && input.startsWith("//");
+        return input != null && input.startsWith(CUSTOM_DELIMITER_PREFIX) && input.contains("\\n");
     }
 
     @Override
     public String[] split(String input) {
-        int customDelimiterIndex = input.indexOf("\\n");
-        String customDelimiter = Pattern.quote(input.substring(2, customDelimiterIndex));
-        input = input.substring(customDelimiterIndex + 2);
+        int customDelimiterIndex = input.indexOf(CUSTOM_DELIMITER_SEPARATOR);
+
+        String customDelimiter = Pattern.quote(
+                input.substring(CUSTOM_DELIMITER_PREFIX.length(), customDelimiterIndex));
+
+        input = input.substring(customDelimiterIndex + CUSTOM_DELIMITER_SEPARATOR.length());
 
         String allDelimiters = DEFAULT_DELIMITERS + "|" + customDelimiter;
 
