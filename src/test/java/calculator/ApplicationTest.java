c41ae97a221a29d8e3b90a85976d746531f7a601
choiyoung69 <choiyoung609@gmail.com>
2025-10-20 21:50:01 +0900

    test: 커스텀 구분자를 감싸는 값이 없을 떄 예외 처리

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -81,6 +81,22 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 커스텀_구분자를_감싸는_값이_없을떄_예외_테스트1() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("+\\n1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_구분자를_감싸는_값이_없을떄_예외_테스트2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//+1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
 
     @Override
     public void runMain() {
