f32d7832cf9015215d596e66eab45efaa51a26f9
choiyoung69 <choiyoung609@gmail.com>
2025-10-20 21:49:16 +0900

    test: 커스텀 구분자가 없을 때 예외 처리

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -73,6 +73,15 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 커스텀_구분자가_없을떄_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//\\n1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
