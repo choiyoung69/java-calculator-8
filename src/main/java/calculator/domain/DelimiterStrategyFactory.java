e44fb1de0e0f0101a123f5f26683279eb71c3a85
choiyoung69 <choiyoung609@gmail.com>
2025-10-20 19:03:59 +0900

    refactor: DelimiterFactory에 전략 추가 및 from 메서드 변경

--- src/main/java/calculator/domain/DelimiterStrategyFactory.java
+++ src/main/java/calculator/domain/DelimiterStrategyFactory.java
@@ -1,11 +1,18 @@
 package calculator.domain;
 
+import java.util.List;
+
 public class DelimiterStrategyFactory {
 
+    private static final List<DelimiterStrategy> strategies = List.of(
+            new CustomDelimiterStrategy(),
+            new DefaultDelimiterStrategy()
+    );
+
     public static DelimiterStrategy from(String input) {
-        if (input.startsWith("//")) {
-            return new CustomDelimiterStrategy();
-        }
-        return new DefaultDelimiterStrategy();
+        return strategies.stream()
+                .filter(strategies -> strategies.canHandle(input))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("적절한 구분자 전략이 없습니다."));
     }
 }
